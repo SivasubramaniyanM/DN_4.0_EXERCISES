5c0917864620e3defb405b5a8d9c6e386f43162e
SivasubramaniyanM <sivasubramaniyanm22it@psnacet.edu.in>
2025-06-20 22:52:42 +0530

    completed

--- Week1_Excercises/Design Pattern and Principle/SingletonPatternExample/code/src/Logger.java
+++ Week1_Excercises/Design Pattern and Principle/SingletonPatternExample/code/src/Logger.java
@@ -1,12 +1,12 @@
 public class Logger {
     private static Logger instance;
 
-    
+    // Private constructor to prevent instantiation
     private Logger() {
         System.out.println("Logger initialized. Ready to record logs!");
     }
 
-   
+    // Public method to get the singleton instance
     public static Logger getInstance() {
         if (instance == null) {
             instance = new Logger();
@@ -14,6 +14,7 @@ public class Logger {
         return instance;
     }
 
+    // Example log method
     public void log(String message) {
         System.out.println("Logging message: \"" + message + "\"");
     }
